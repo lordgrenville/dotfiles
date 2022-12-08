86807f910e906a587537699d476be950b2eaecdd
Josh Friedlander <16547083+lordgrenville@users.noreply.github.com>
2022-12-08 11:53:16 +0200

    update doom config

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -33,7 +33,8 @@
    flycheck-global-modes '(not gfm-mode forge-post-mode gitlab-ci-mode dockerfile-mode Org-mode org-mode)
    conda-anaconda-home "~/miniforge3/"
 ; does this work? if not use M-x ispell-change-dict
-   ;; ispell-dictionary "en_ZA"
+   ispell-dictionary "en_ZA"
+   ;; ispell-local-dictionary "en_ZA"
    ;; ispell-program-name "aspell"
    ;; ispell-extra-args '("--sug-mode=ultra" "--run-together")
    org-todo-keywords '((sequence "TODO" "DONE"))
@@ -81,11 +82,22 @@
   (evil-ex-search-next 1)
   (evil-scroll-line-to-center nil))
 
+(evil-define-command evil-goto-mark-line (char &optional noerror)
+  "Go to the line of the marker specified by CHAR."
+  :keep-visual t
+  :repeat nil
+  :type line
+  :jump t
+  (interactive (list (read-char)))
+  (evil-goto-mark char noerror)
+  (evil-first-non-blank)
+  (evil-scroll-line-to-center nil))
+
 (map!
  (:after evil
-   :n "z=" #'flyspell-correct-word-before-point
-   :n "n"  #'next-search-and-centre
-   )
+  :n "z=" #'flyspell-correct-word-before-point
+  :n "n"  #'next-search-and-centre
+  )
  )
 
 ;; keybind to disable search highlighting (like :set noh)
@@ -93,20 +105,6 @@
       :desc "Clear search highlight"
       "s c"
       #'evil-ex-nohighlight)
-
-;; (map! :n
-;;       "C-a" #'evil-numbers/inc-at-pt
-;;       "C-x" #'evil-numbers/dec-at-pt
-;; )
-
-; (map!
-  ; :after company
-  ; :map company-active-map
-  ;; "RET" nil
-  ;; "<return>" nil
-  ;; [tab] nil
-  ; "TAB" nil
-  ; )
 
 (after! org
   (setq org-startup-folded t)
@@ -128,18 +126,12 @@
 (after! treemacs
   (setq treemacs-git-mode nil))
 
-(defun add-pcomplete-to-capf ()
-  (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point nil t))
-
 (add-hook! 'org-mode-hook
            #'turn-off-smartparens-mode
            ;; #'add-pcomplete-to-capf
            'org-fragtog-mode)
-;; (define-key global-map (kbd "C-c x")
-;;   (lambda () (interactive) (org-capture nil "t")))
 
 ; lines should be the screen length of my MBP, not 80 (emacs default) or 70 (org-mode default!)
-;
 ;; (setq-hook! '(text-mode-hook) fill-column 145)
 
 ;; (after! treemacs
@@ -147,10 +139,14 @@
 ;;         treemacs-show-cursor nil
 ;;         treemacs-width 30))
 
-(add-hook! python-mode (conda-env-activate "myenv"))
-; pylsp formatter doesn't work, overrride it manually
+(add-hook! python-mode
+  (conda-env-activate "myenv")
+  (lambda () (+fold/close-all))
+)
+
 (setq-hook! 'python-mode-hook
   +format-with 'black
+; pylsp formatter doesn't work, overrride it manually
   lsp-pylsp-plugins-pydocstyle-enabled nil
   lsp-pylsp-plugins-flake8-config "/Users/josh/.flake8"
   )
@@ -159,16 +155,6 @@
 (remove-hook 'text-mode-hook #'spell-fu-mode)
 (add-hook 'text-mode-hook #'flyspell-mode)
 
-;; (evil-set-initial-state 'term-mode 'emacs)
-;; (evil-set-initial-state 'vterm-mode 'emacs)
-;; (evil-set-initial-state 'help-mode 'emacs)
-;; (evil-set-initial-state 'shell-mode 'emacs)
-;; (evil-set-initial-state 'dired-mode 'emacs)
-;; (evil-set-initial-state 'wdired-mode 'normal)
-;; (evil-set-initial-state 'git-commit-mode 'emacs)
-;; (evil-set-initial-state 'git-rebase-mode 'emacs)
-
-;; the above was rewritten into a loop using the dolist macro!
 (dolist (mode '(
                    ;; term-mode
                    ;; vterm-mode
@@ -176,8 +162,8 @@
                    ;; shell-mode
                    ;; dired-mode
                    ;; wdired-mode
-                   git-commit-mode
-                   git-rebase-mode
+                   ;; git-commit-mode
+                   ;; git-rebase-mode
                    ))
   (evil-set-initial-state mode 'emacs))
 
@@ -211,6 +197,7 @@
       :map evil-normal-state-map
       "z=" #'flyspell-correct-wrapper
       )
+
 (evil-define-key 'normal peep-dired-mode-map (kbd "<SPC>") 'peep-dired-scroll-page-down
                                              (kbd "C-<SPC>") 'peep-dired-scroll-page-up
                                              (kbd "<backspace>") 'peep-dired-scroll-page-up
@@ -230,116 +217,6 @@
          ;; File names ending with # or ~
          "__pycache__"))
 
-(add-hook 'python-mode-hook
-          (lambda ()
-            (+fold/close-all)
-            ))
-
-;; (add-hook 'text-mode-hook 'turn-on-auto-fill)
-;; (add-hook 'org-mode-hook 'turn-off-auto-fill)
-
-;; (after! treemacs
-;;   (setq treemacs-default-visit-action 'treemacs-visit-node-close-treemacs))
-
-;; (after! org-capture
-
-;;   (defun +doct-icon-declaration-to-icon (declaration)
-;;     "Convert :icon declaration to icon"
-;;     (let ((name (pop declaration))
-;;           (set  (intern (concat "all-the-icons-" (plist-get declaration :set))))
-;;           (face (intern (concat "all-the-icons-" (plist-get declaration :color))))
-;;           (v-adjust (or (plist-get declaration :v-adjust) 0.01)))
-;;       (apply set `(,name :face ,face :v-adjust ,v-adjust))))
-
-;;   (defun +doct-iconify-capture-templates (groups)
-;;     "Add declaration's :icon to each template group in GROUPS."
-;;     (let ((templates (doct-flatten-lists-in groups)))
-;;       (setq doct-templates (mapcar (lambda (template)
-;;                                      (when-let* ((props (nthcdr (if (= (length template) 4) 2 5) template))
-;;                                                  (spec (plist-get (plist-get props :doct) :icon)))
-;;                                        (setf (nth 1 template) (concat (+doct-icon-declaration-to-icon spec)
-;;                                                                       "\t"
-;;                                                                       (nth 1 template))))
-;;                                      template)
-;;                                    templates))))
-
-;;   (setq doct-after-conversion-functions '(+doct-iconify-capture-templates))
-
-;;   (defun set-org-capture-template ()
-;;     (setq org-capture-templates
-;;           (doct `(("Personal todo" :keys "t"
-;;                    :icon ("checklist" :set "octicon" :color "green")
-;;                    :file +org-capture-todo-file
-;;                    :prepend t
-;;                    :headline "Inbox"
-;;                    :type entry
-;;                    :template ("* TODO %?"
-;;                               "%i"))
-;;                   ("Personal note" :keys "n"
-;;                    :icon ("sticky-note-o" :set "faicon" :color "green")
-;;                    :file +org-capture-todo-file
-;;                    :prepend t
-;;                    :headline "Inbox"
-;;                    :type entry
-;;                    :template ("* %?"
-;;                               "%i %a"))
-;;                   ("Tasks" :keys "k"
-;;                    :icon ("inbox" :set "octicon" :color "yellow")
-;;                    :file +org-capture-todo-file
-;;                    :prepend t
-;;                    :headline "Tasks"
-;;                    :type entry
-;;                    :template ("* TODO %? %^G%{extra}"
-;;                               "%i %a")
-;;                    :children (("General Task" :keys "k"
-;;                                :icon ("inbox" :set "octicon" :color "yellow")
-;;                                :extra "")
-;;                               ("Task with deadline" :keys "d"
-;;                                :icon ("timer" :set "material" :color "orange" :v-adjust -0.1)
-;;                                :extra "\nDEADLINE: %^{Deadline:}t")
-;;                               ("Scheduled Task" :keys "s"
-;;                                :icon ("calendar" :set "octicon" :color "orange")
-;;                                :extra "\nSCHEDULED: %^{Start time:}t")))
-;;                   ))))
-;;   (set-org-capture-template)
-;;   (unless (display-graphic-p)
-;;     (add-hook 'server-after-make-frame-hook
-;;               (defun org-capture-reinitialise-hook ()
-;;                 (when (display-graphic-p)
-;;                   (set-org-capture-templates)
-;;                   (remove-hook 'server-after-make-frame-hook
-;;                                #'org-capture-reinitialise-hook))))))
-
-;; (defun org-capture-select-template-prettier (&optional keys)
-;;   "Select a capture template, in a prettier way than default
-;; Lisp programs can force the template by setting KEYS to a string."
-;;   (let ((org-capture-templates
-;;          (or (org-contextualize-keys
-;;               (org-capture-upgrade-templates org-capture-templates)
-;;               org-capture-templates-contexts)
-;;              '(("t" "Task" entry (file+headline "" "Tasks")
-;;                 "* TODO %?\n  %u\n  %a")))))
-;;     (if keys
-;;         (or (assoc keys org-capture-templates)
-;;             (error "No capture template referred to by \"%s\" keys" keys))
-;;       (org-mks org-capture-templates
-;;                "Select a capture template\n━━━━━━━━━━━━━━━━━━━━━━━━━"
-;;                "Template key: "
-;;                `(("q" ,(concat (all-the-icons-octicon "stop" :face 'all-the-icons-red :v-adjust 0.01) "\tAbort")))))))
-;; (advice-add 'org-capture-select-template :override #'org-capture-select-template-prettier)
-
-;; (add-hook 'tuareg-mode-hook #'(lambda() (setq mode-name "🐫")))
-
-;; (add-to-list 'load-path "/Users/josh/.opam/default/share/emacs/site-lisp")
-;; (require 'ocp-indent)
-
-; visual line numbers that work with folds
-;; (defun josh/toggle-relative-line-numbers ()
-;;   (interactive)
-;;   (if display-line-numbers
-;;       (setq display-line-numbers nil)
-;;     (progn (setq display-line-numbers 'visual)
-;;            )))
 ; Here are some additional functions/macros that could help you configure Doom:
 ;
 ; - `load!' for loading external *.el files relative to this one
